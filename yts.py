bb10f33707dd2ca345acb7a86e65784b992ee5d9
lawlie8 <50700194+lawlie8@users.noreply.github.com>
2019-12-14 19:18:53 +0530

    /

--- yts.py
+++ yts.py
@@ -4,59 +4,87 @@ import time
 import sys
 import os
 
-
-
-
 tmp_list = []   #list to store magnet links
-x = open('yts_magnet.txt','r+')
-hu = x.readlines()
+dir_files = []
+error_lines2 = []
+tmp_list2 = []
+for files in os.listdir():
+    dir_files.append(files)
+    if files == 'error.txt':
+        print(files)
+        error_file = open('error.txt','r+')
+        error_lines = error_file.readlines()
+        i = error_lines[0]
+        i = i[7]
+        i = int(i)
+        for ljokj in range(0,len(error_lines)):
+            error_lines2.append(error_lines[ljokj].strip('\n'))
+        error_lines.clear()
+        for error_links in error_lines2[4:]:
+            tmp_list.append(error_links)
+        #############opti##########
+        '''
+        i1,i2,i3 = 0,0,1
+        print(len(tmp_list))
+        while True:
+            if i1 <= len(tmp_list):
+                try:
+                    tmp_list2.append(tmp_list[i2] + tmp_list[i3])
+                    #print(tmp_list2)
+                    i1 = i1 + 1
+                    i2 = i2 + 2
+                    i3 = i3 + 2
+                except:
+                    break
+        tmp_list = tmp_list2
+        print(tmp_list2)
+        '''
+alshdjad = input()
+
 try:
-    list = hu[3].split('title=')
-    str = list[-1].split('"')
-    last = str[1]
+    x = open('yts_magnet.txt','r+')
+    hu = x.readlines()
+    try:
+        list = hu[3].split('title=')
+        str = list[-1].split('"')
+        last = str[1]
+    except:
+        list = hu[6].split('title=')
+        str = list[-1].split('"')
+        last = str[1]
+
+    print('Found last magnet link at line 3  ' + last)
 except:
-    list = hu[6].split('title=')
-    str = list[-1].split('"')
-    last = str[1]
+    print('Check yts_magnet.txt')
+    time.sleep(2)
+    exit()
 
-print('Found last magnet link at line 3  ' + last)
-
-
-
-def check(no_time_to_think_about_new_variable,last):
-
+def check(no_time_to_think_about_new_variable,last):#check Function to check last magnet link
     if no_time_to_think_about_new_variable.split(' ') == last.split(' '):
         fw = open('yts_magnet.txt','r+')
         con = fw.readlines()
         open('yts_magnet.txt','w').close()
         fw = open('yts_magnet.txt','w+')
-
         for run in tmp_list:
             if len(tmp_list) > 1:
-
                 fw.write(run)
-
             else:
                 for hj in con:
                     fw.write(hj)
                 print("Your List is up to date \nExiting yts.py")
                 time.sleep(3)
+                os.system('del error.txt')
                 os.system('taskkill  /f /im  py.exe')
         fw = open('yts_magnet.txt','a')
-
         for hj in con:
-
             fw.write(hj)
         print('Last Magnet Reached')
         time.sleep(5)
         os.system('taskkill  /f /im  py.exe')
         fw.close()
-
-
 for i in range(1,2000):  #why 2000? just because
     print("\n\n{}\n\n".format(i))
     def first_page(source , soup):
-
         for movie_link in soup.find_all('a',class_='browse-movie-link', href=True):
             link_visit = movie_link['href']
             source1 = requests.get(link_visit).text
@@ -73,21 +101,23 @@ for i in range(1,2000):  #why 2000? just because
             except:
                 print("Error at magnet link side")
             try:
-                #no_time_to_think_about_new_variable = magnet_link['href']
                 print(no_time_to_think_about_new_variable)
-                #print('\n' + print_data)
             except:
                 print("NONE")
-
     source = requests.get("https://yts.lt/browse-movies?page={}".format(i)).text
     soup = BeautifulSoup(source,features='html.parser')
-
     if source == "":
         print('end of the line mate\nBYE BYE ')
         fw.close()
         time.sleep(5)
         os.system('taskkill  /f /im  yts.exe')
-
-
-    #print("Error with Get Requests")
-    first_page(source ,soup)
+    try:
+        first_page(source ,soup)
+    except:
+        print('\n\nErrot Occured creating errror file\n\n')
+        time.sleep(2)
+        error_file = open('error.txt','w+')
+        error_file.write('page = {}\n\n'.format(i))
+        for magnet_link in tmp_list:
+            error_file.write(magnet_link)
+            print(magnet_link)
